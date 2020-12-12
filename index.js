1943121ca89c122bce916358e5641ace5a311ce4
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 10:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ class Formatter {
  
 static titleize(string){
 const words = string.split(" ");
-for(let i = 0; i < words.length; i++) {
+words[0].charAt(0).toUpperCase() + string.slice(1);
+for(let i = 1; i < words.length; i++) {
 if(words[i]==="the" || words[i]==="a" || words[i]==="an" || words[i]==="but" || words[i]==="of" || words[i]==="and" || words[i]==="for" || words[i]==="at" || words[i]==="by" || words[i]==="from"){
   words[i] = words[i][0].toLowerCase() + words[i].substr(1);
 }else{
@@ -27,19 +28,6 @@ return words.join(" ");
 
 
 /*describe( "Formatter", () => {
-
-  describe( "titleize", () => {
-    it( "is a static method", () => {
-      expect( Formatter.titleize( "F forever foolish" ) ).to.not.throw
-    } )
-
-    it( "capitalizes all words in a sentence except 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'", () => {
-      expect( Formatter.titleize( "getting giggles" ) ).to.equal( "Getting Giggles" )
-      expect( Formatter.titleize( "where the wild things are" ) ).to.equal( "Where the Wild Things Are" )
-      expect( Formatter.titleize( "chicken soup with rice and a few other songs" ) ).to.equal( "Chicken Soup With Rice and a Few Other Songs" )
-      expect( Formatter.titleize( "Maurice a an but of and for at by from end" ) ).to.equal( "Maurice a an but of and for at by from End" )
-    } )
-
     it( "always capitalizes the first word", () => {
       expect( Formatter.titleize( "a tale of two cities" ) ).to.equal( "A Tale of Two Cities" )
       expect( Formatter.titleize( "in the night kitchen" ) ).to.equal( "In the Night Kitchen" )
