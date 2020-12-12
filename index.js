8ebca6d00d0b8f4c5c95bfa7069d1a927c014b2f
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 09:28:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+   static sanitize(string){
+    return string.replace(/[&\/\\#,+()$~%.!@^":*?<>{}]/g, '');
+  }
+ 
+   static titleize(string){
+   
+  }
+
 }
+
+
+
+
+
+/*describe( "Formatter", () => {
+
+  describe( "titleize", () => {
+    it( "is a static method", () => {
+      expect( Formatter.titleize( "F forever foolish" ) ).to.not.throw
+    } )
+
+    it( "capitalizes all words in a sentence except 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'", () => {
+      expect( Formatter.titleize( "getting giggles" ) ).to.equal( "Getting Giggles" )
+      expect( Formatter.titleize( "where the wild things are" ) ).to.equal( "Where the Wild Things Are" )
+      expect( Formatter.titleize( "chicken soup with rice and a few other songs" ) ).to.equal( "Chicken Soup With Rice and a Few Other Songs" )
+      expect( Formatter.titleize( "Maurice a an but of and for at by from end" ) ).to.equal( "Maurice a an but of and for at by from End" )
+    } )
+
+    it( "always capitalizes the first word", () => {
+      expect( Formatter.titleize( "a tale of two cities" ) ).to.equal( "A Tale of Two Cities" )
+      expect( Formatter.titleize( "in the night kitchen" ) ).to.equal( "In the Night Kitchen" )
+    } )
+  } )
+} )*/
