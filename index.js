b8583709e00b82f2b86e35a02bc9e5a01397dcc8
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 10:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ class Formatter {
  
 static titleize(string){
 const words = string.split(" ");
-words[0].charAt(0).toUpperCase() + string.slice(1);
+
 for(let i = 1; i < words.length; i++) {
+  words[0] = words[0][0].toUpperCase() + words[0].substr(1);
 if(words[i]==="the" || words[i]==="a" || words[i]==="an" || words[i]==="but" || words[i]==="of" || words[i]==="and" || words[i]==="for" || words[i]==="at" || words[i]==="by" || words[i]==="from"){
   words[i] = words[i][0].toLowerCase() + words[i].substr(1);
 }else{
