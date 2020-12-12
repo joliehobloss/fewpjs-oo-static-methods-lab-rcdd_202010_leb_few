ecadf9632b03ea1ff63d8295caf0ea3a7fce8c40
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 09:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Formatter {
    static titleize(string){
 const words = string.split(" ");
 for(let i = 0; i < words.length; i++) {
-  if(words[i].indexOf("the")===1 || words[i].indexOf("a")===1 || words[i].indexOf("an")===1 || words[i].indexOf("but")===1 || words[i].indexOf("of")===1 || words[i].indexOf("and")===1 || words[i].indexOf("for")===1 || words[i].indexOf("at")===1 || words[i].indexOf("by")===1 || words[i].indexOf("from")===1){
+  if(words[i].indexOf("the")===1 && words[i].indexOf("a")===1 && words[i].indexOf("an")===1 && words[i].indexOf("but")===1 && words[i].indexOf("of")===1 && words[i].indexOf("and")===1 && words[i].indexOf("for")===1 && words[i].indexOf("at")===1 && words[i].indexOf("by")===1 && words[i].indexOf("from")===1){
   words[i] = words[i][0].toLowerCase() + words[i].substr(1);
 }else{
   words[i] = words[i][0].toUpperCase() + words[i].substr(1);
