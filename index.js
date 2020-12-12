6370812e2bc9e38bb2dde4077d56f6d54728b10d
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 10:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Formatter {
 static titleize(string){
 const words = string.split(" ");
 for(let i = 0; i < words.length; i++) {
-if(words[i]==="the" || words[i]==="a" ){
+if(words[i]==="the" || words[i]==="a" || words[i]==="an" || ){
   words[i] = words[i][0].toLowerCase() + words[i].substr(1);
 }else{
   words[i] = words[i][0].toUpperCase() + words[i].substr(1);
