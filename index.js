d2615131fd9436b5c28de84337f393a1a9d89b34
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 09:33:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ class Formatter {
   }
  
    static titleize(string){
-   const words = string.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+const words = string.split(" ");
+for(let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
 }
+return words.join(" ");
   }
 
 }
